49bf6e4fa263224db0f6d365d65ae1ffecc5f0fb
Ian Ohlander <Ian.Ohlander@phoenix.gov>
2022-11-08 09:27:39 -0700

    fixed formatting of mandelbrot

--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -1,71 +1,71 @@
-let s=375;
-let max=2;
-let numIterations=60;
-let x1off=0;
-let y1off=1000;
-let x2off=1000000;
-let y2off=10000000000;
-let ca=0;
-let cb=0;
-let zx=0;
-let zy=0;
-let counter1=0.008;
-let counter2=0.008;
-let counter3=0.1;
-let counter4=0.01;
-let beginZoom=1.0;
-let zoomlevel=beginZoom/5;
-let hoffset=.5;
-let voffset=.5;
-let leftedge=(-zoomlevel)-hoffset;
-let rightedge=(zoomlevel)-hoffset;
-let topedge=(zoomlevel)-voffset;
-let bottomedge=(-zoomlevel)-voffset;
+let s = 375;
+let max = 2;
+let numIterations = 60;
+let x1off = 0;
+let y1off = 1000;
+let x2off = 1000000;
+let y2off = 10000000000;
+let ca = 0;
+let cb = 0;
+let zx = 0;
+let zy = 0;
+let counter1 = 0.008;
+let counter2 = 0.008;
+let counter3 = 0.1;
+let counter4 = 0.01;
+let beginZoom = 1.0;
+let zoomlevel = beginZoom / 5;
+let hoffset = .5;
+let voffset = .5;
+let leftedge = (-zoomlevel) - hoffset;
+let rightedge = (zoomlevel) - hoffset;
+let topedge = (zoomlevel) - voffset;
+let bottomedge = (-zoomlevel) - voffset;
 
 const colorsRed = [];
 const colorsGreen = [];
 const colorsBlue = [];
 
-function setup(){
-    let mycanvas=createCanvas(s,s);
+function setup() {
+    let mycanvas = createCanvas(s, s);
     colorMode(HSB, 1);
     mycanvas.parent('mandelbrot');
     createColors();
 }
 
-function createColors(){
+function createColors() {
     for (let n = 0; n < numIterations; n++) {
         let hu = sqrt(n / numIterations);
         let col = color(hu, 255, 100);
         colorsRed[n] = red(col);
         colorsGreen[n] = green(col);
         colorsBlue[n] = blue(col);
-      }
+    }
 }
 
 
-function draw(){
+function draw() {
     pixelDensity(1);
     loadPixels();
-    x1off+=counter1;
-    y1off+=counter2;
+    x1off += counter1;
+    y1off += counter2;
     //x2off+=counter3;
     //y2off+=counter4;
-    let noisex1=noise(x1off);
-    let noisey1=noise(y1off);
+    let noisex1 = noise(x1off);
+    let noisey1 = noise(y1off);
     //let noisex2=noise(x2off);
     //let noisey2=noise(y2off);
-    for(var x=0;x<width;x++){
-        for(var y=0;y<height;y++){
+    for (var x = 0; x < width; x++) {
+        for (var y = 0; y < height; y++) {
             //noisex=noise(x1off);
             //noisey=noise(y1off);
             //zx=map(noisex1,0,1,-max,max);
             //zy=map(noisey1,0,1,-max,max);
-            var a=map(x,0,width,leftedge,rightedge);
-            var b=map(y,0,height,topedge,bottomedge);
+            var a = map(x, 0, width, leftedge, rightedge);
+            var b = map(y, 0, height, topedge, bottomedge);
             //var a=map(x,0,width,zx,zy);
             //var b=map(y,0,height,zx,zy);
-            
+
             //console.log(noise(x));
             /*if(mouseX!=oldx){
                 console.log(mouseX);
@@ -77,44 +77,44 @@ function draw(){
             }*/
             //ca=map(mouseX,0,width, -1,1);
             //cb=map(mouseY,0,height, -1,1);
-            
-            ca=map(noisex1,0,1, -1,1);
-            cb=map(noisey1,0,4, -1,1);
-            var n=0;
-            var z=0;
 
-            while(n<numIterations){
-                var aa=a*a-b*b;
-                var bb=2*a*b;
+            ca = map(noisex1, 0, 1, -1, 1);
+            cb = map(noisey1, 0, 4, -1, 1);
+            var n = 0;
+            var z = 0;
 
-                a=aa+ca;
-                b=bb+cb;
+            while (n < numIterations) {
+                var aa = a * a - b * b;
+                var bb = 2 * a * b;
 
-                if(a+b>16){
+                a = aa + ca;
+                b = bb + cb;
+
+                if (a + b > 16) {
                     break;
                 }
                 n++;
             }
-            
-            var bright=map(n,0,numIterations,0,255);
 
-            var pix=(x+(y*width))*4;
+            var bright = map(n, 0, numIterations, 0, 255);
+
+            var pix = (x + (y * width)) * 4;
             /*pixels[pix+0]=bright;
             pixels[pix+1]=bright;
             pixels[pix+2]=bright;*/
-            
+
             if (n == numIterations) {
                 pixels[pix + 0] = 0;
                 pixels[pix + 1] = 0;
                 pixels[pix + 2] = 0;
-              } 
-              else {
+            }
+            else {
                 // Otherwise, use the colors that we made in setup()
                 pixels[pix + 0] = colorsRed[n];
                 pixels[pix + 1] = colorsGreen[n];
                 pixels[pix + 2] = colorsBlue[n];
             }
-            pixels[pix+3]=255;
+            pixels[pix + 3] = 255;
         }
     }
     updatePixels();
